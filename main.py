f157cfdf9c46ffdecd99d75e0f23540ccf14fc51
* <aribidarajerry@gmail.com>
2025-05-19 16:33:31 +0100

    initial commit

--- main.py
+++ main.py
@@ -7,14 +7,26 @@ load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
+"""
+{% extends 'base.html' %}
+{% block _style %}{% endblock %}
+{% block title %}{% endblock %}
+{% block landing %}{% endblock %}
+{% block content %}{% endblock %}
+
+"""
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/login')
-def login():
-    return render_template('base.html')
+@app.route('/2')
+def template_2():
+    return render_template('index2.html')
+
+@app.route('/3')
+def template_3():
+    return render_template('index3.html')
 
 @app.route('/register')
 def register():
@@ -23,6 +35,10 @@ def register():
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('error.html', error=e), 404
+
+@app.errorhandler(405)
+def method_not_allowed(e):
+    return render_template('error.html', error=e), 405
 
 @app.errorhandler(500)
 def internal_server_error(e):
